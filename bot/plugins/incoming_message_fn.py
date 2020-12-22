bc028ab6ee6fc1b7343d1f959d531c40f27280cc
果汁 <71187213+FristyFlakes@users.noreply.github.com>
2020-12-22 13:21:04 +0100

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -58,10 +58,10 @@ async def incoming_start_message_f(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Updates Channel', url='https://t.me/Discovery_Updates')
+                    InlineKeyboardButton('Updates Channel 📢', url='https://t.me/FlixBots')
                 ],
                 [
-                    InlineKeyboardButton('Support Group', url='https://t.me/linux_repo')
+                    InlineKeyboardButton('Support Group 🗣', url='https://t.me/MirrorZone')
                 ]
             ]
         ),
