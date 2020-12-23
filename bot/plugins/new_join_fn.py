9cbb9cde3f9eb3d8d6fd2b539e6302a4a23d91e0
果汁 <71187213+FristyFlakes@users.noreply.github.com>
2020-12-23 21:17:10 +0100

    Update new_join_fn.py

--- bot/plugins/new_join_fn.py
+++ bot/plugins/new_join_fn.py
@@ -29,10 +29,14 @@ async def help_message_f(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Updates Channel 📢', url='https://t.me/FlixBots')
+                    InlineKeyboardButton('Updates Channel', url='https://t.me/Discovery_Updates')
                 ],
                 [
-                    InlineKeyboardButton('Support Group 🗣', url='https://t.me/MirrorZone')
+                    InlineKeyboardButton('Support Group', url='https://t.me/linux_repo')
+                ],
+                [
+                    InlineKeyboardButton('Developer', url='https://t.me/AbirHasan2005'), # Bloody Thief, Don't Become a Developer by Stealing other's Codes & Hard Works!
+                    InlineKeyboardButton('Source Code', url='https://github.com/AbirHasan2005/VideoCompress') # Must Give us Credits!
                 ]
             ]
         ),
